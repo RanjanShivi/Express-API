7ba1205a0cdb9d11089f37e2e90624db1024cc47
Shivangi Ranjan <shivangiec1098@gmail.com>
2022-02-20 23:28:58 +0530

    [Shivangi]-Contains list of Mongoose-Querry

---
+++ mongoose-querry.js
@@ -0,0 +1,18 @@
+// Mongoose models provide several static helper functions for CRUD operations.
+// Each of these functions returns a mongoose Query object.
+
+Model.deleteMany()
+Model.deleteOne()
+Model.find()
+Model.findById()
+Model.findByIdAndDelete()
+Model.findByIdAndRemove()
+Model.findByIdAndUpdate()
+Model.findOne()
+Model.findOneAndDelete()
+Model.findOneAndRemove()
+Model.findOneAndReplace()
+Model.findOneAndUpdate()
+Model.replaceOne()
+Model.updateMany()
+Model.updateOne()
